b5fe8d325f0066b936185ceeb85dce992a75de56
Aliaksandr Semianiyuk <a_semianiyuk@by1-al-100.corp.wargaming.local>
2018-12-05 13:40:30 +0300

    Added heroku host to allowed hosts

--- lyte_tech_challenge/settings.py
+++ lyte_tech_challenge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vepuwt!##^0mqaw*9bp$=tgimja(ka(8c@%aq=m!k4l34t$hrx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lyte-tech-challenge.herokuapp.com', ]
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles)')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
